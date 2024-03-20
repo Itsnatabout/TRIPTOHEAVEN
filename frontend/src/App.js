5fdda88893dd4374f9d0f540424a41ecfe45b145
natswell <128900281+natswell@users.noreply.github.com>
2024-03-21 01:22:57 +0800

    frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState} from 'react'
+
+const App = () => {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+      
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  // this returns what you want the client to see
+  return (
+    <div>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading....</p>
+      ): (
+          backendData.users.map((user, i) => (
+            <p key={i}>{user}</p>
+        ))        
+      )}
+
+    </div>
+  )
+}
+
+export default App
