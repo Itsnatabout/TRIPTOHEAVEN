abcd10873e68743edb0397e775782b0cd530a033
natswell <128900281+natswell@users.noreply.github.com>
2024-03-21 01:03:24 +0800

    express react setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+// express app
+const app = express()
+
+
+//rout for api
+app.get("/api", (req, res) => {
+    res.json({
+        "users": ["userOne", "userTwo", "userThree", "userFour", "userFive"]
+    }) //only for test
+})
+
+// listen for requests
+app.listen(5000, () => {
+    console.log('server listening on port 5000')
+
+})
